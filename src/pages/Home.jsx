48d9eb301ba35c3887377075a2ad2148025e0b06
Alex Nicolas <alexoliveira604@gmail.com>
2023-10-31 17:21:34 -0300

    feat: user should see input when access homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 export function HomePage () {
     return (
         <section>
-            testando
+            <form class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">@</span>
+                <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Type a GitHub username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                />
+            </form>
         </section>
     )
 }
