ac996cdc4ced071f2633900a47096d882aa6d62c
Alex Nicolas <alexoliveira604@gmail.com>
2023-10-31 17:18:58 -0300

    feat: bootstrap was configured

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
   },
   plugins: [react()],
 })
