1752487eaf33c4c86f459cd191f543c8485ab0b3
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-01 12:01:11 -0300

    feat: user should see loading instead of repository list component when browser is getting data from github api

--- src/component/RepositoryList.jsx
+++ src/component/RepositoryList.jsx
@@ -1,13 +1,15 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 import { Repository } from "./Repository"
 import { useUser } from "../hook"
+import { Loading } from "./Loading"
 
 export const RepositoryList = () => {
     const { username } = useParams()
     const { repositories, setRepositories, isUserFound } = useUser()
+    const [isLoading , setIsLoading] = useState(false)
 
     useEffect(() => {
         if (isUserFound) {
@@ -16,7 +18,9 @@ export const RepositoryList = () => {
     }, [isUserFound])
 
     const fetchRepositories = async () => {
+        setIsLoading(true)
         const response = await axios.get(`https://api.github.com/users/${username}/repos`)
+        setIsLoading(false)
         const repositoriesReduced = response.data.map((repository) => ({
             name: repository.name,
             starsCount: repository.stargazers_count,
@@ -27,7 +31,8 @@ export const RepositoryList = () => {
 
     return (
         <ul>
-            {repositories.map((repository, index) => 
+            {isLoading && <Loading />}
+            {!isLoading && repositories.map((repository, index) => 
                 <Repository
                     name={repository.name}
                     starsCount={repository.starsCount}
