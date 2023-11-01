9e1c27b31b1bfaec4715a27dcabe9626579eae40
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-01 09:22:28 -0300

    feat: user should be sent to user result search page when submit form to do show github profile data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react"
+import { useNavigate } from "react-router"
+
 export function HomePage () {
+    const navigate = useNavigate()
+    const [username, setUsername] = useState()
+
+    const handleSubmit = ($event) => {
+        $event.preventDefault()
+        navigate(`/user/${username}`)
+    }
+
     return (
         <section>
-            <form class="input-group mb-3">
+            <form class="input-group mb-3" onSubmit={handleSubmit}>
                 <span class="input-group-text" id="basic-addon1">@</span>
                 <input
                     type="text"
@@ -9,6 +20,8 @@ export function HomePage () {
                     placeholder="Type a GitHub username"
                     aria-label="Username"
                     aria-describedby="basic-addon1"
+                    onChange={($event) => setUsername($event.target.value)}
+                    value={username}
                 />
             </form>
         </section>
