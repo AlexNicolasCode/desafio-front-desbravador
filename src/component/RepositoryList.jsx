94f00451d0105991d26f1a2b5024028bc273a6c3
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-01 11:32:04 -0300

    feat: RepoistoryList component was updated to fetch user repositories internaly and preserve component context

--- src/component/RepositoryList.jsx
+++ src/component/RepositoryList.jsx
@@ -1,6 +1,30 @@
-import { Repository } from "./Repository"
+import axios from "axios"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
 
-export const RepositoryList = ({ repositories }) => {
+import { Repository } from "./Repository"
+import { useUser } from "../hook"
+
+export const RepositoryList = () => {
+    const { username } = useParams()
+    const { repositories, setRepositories, isUserFound } = useUser()
+
+    useEffect(() => {
+        if (isUserFound) {
+            fetchRepositories()
+        }
+    }, [isUserFound])
+
+    const fetchRepositories = async () => {
+        const response = await axios.get(`https://api.github.com/users/${username}/repos`)
+        const repositoriesReduced = response.data.map((repository) => ({
+            name: repository.name,
+            starsCount: repository.stargazers_count,
+        }))
+        const reorderedRepositories = repositoriesReduced.sort((a, b) => b.starsCount - a.starsCount)
+        setRepositories(reorderedRepositories)
+    }
+
     return (
         <ul>
             {repositories.map((repository, index) => 
