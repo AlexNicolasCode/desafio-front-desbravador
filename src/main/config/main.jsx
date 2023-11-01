4108adf3cf9505b786150aa5bef0a0d3cb45be7a
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-01 17:43:33 -0300

    refactor: update main files path to preserve project architecture

--- src/main/config/main.jsx
+++ src/main/config/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
-import { router } from './routes'
+import { router } from '@/main/routes'
 
-import '../style/bootstrap.scss'
+import '@/style/bootstrap.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/main/routes/index.jsx
+++ src/main/routes/index.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { HomePage, UserPage } from "../pages"
+import { HomePage, UserPage } from "@/pages"
 
 export const router = createBrowserRouter([
     {
