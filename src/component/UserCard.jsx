b6a545d440e03f89ef4e2dd934c5cb9cdb498a51
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-01 11:32:39 -0300

    feat: UserCard component was updated to fetch user profile data internaly and preserve component context

---
+++ src/component/UserCard.jsx
@@ -0,0 +1,62 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
+import { useUser } from "../hook"
+
+export const UserCard = () => {
+    const { username } = useParams()
+    const { isUserFound, setIsUserFound } = useUser()
+    const [user, setUser] = useState({
+        name: '',
+        bio: '',
+        email: '',
+        avatar_url: '',
+        followersCount: 0,
+        followingCount: 0,
+    })
+
+    useEffect(() => {
+        fetchUser()
+    }, [username])
+    
+    const fetchUser = async () => {
+        const response = await axios.get(`https://api.github.com/users/${username}`)
+        const notFound = 404
+        if (response.status === notFound) {
+            return
+        }
+        const { name, bio, email, avatar_url, followers, following  } = response.data
+        setUser({
+            name,
+            bio,
+            email,
+            avatar_url,
+            followersCount: followers,
+            followingCount: following,
+        })
+        setIsUserFound(true)
+    }
+
+    const renderEmptyState = () => (
+        <section className="card">
+            {username} Not Found on GitHub database
+        </section> 
+    )
+
+    const renderProfileCard = () => (
+        <section className="card">
+            <img src={user.avatar_url} className="card-img-top" alt={username} />
+            <section className="card-body">
+                <h5 className="card-title">{user.name}</h5>
+                <h5 className="card-title">{user.email}</h5>
+                <p className="card-text">{user.bio}</p>
+                <p className="card-text">
+                    Followed: {user.followersCount} Following: {user.followingCount}
+                </p>
+            </section>
+        </section>
+    )
+
+    return isUserFound ? renderProfileCard() : renderEmptyState()
+}
